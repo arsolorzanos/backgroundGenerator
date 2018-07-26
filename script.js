db8d5f8e0973fad3f770453dda81556daa10f278
arsolorzanos <39227098+arsolorzanos@users.noreply.github.com>
2018-07-26 00:38:30 -0500

    npm practice changes

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+var _ = require('lodash');
+var array = [1,342354,6353,23,23,234,5,3,3333,3,3,3,3,3,3,3,3];
+console.log(_.without(array,3));
+
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
